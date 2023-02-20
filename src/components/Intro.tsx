7a7253cb05cf69949579d7de42dc6e2673986adf
abrasumente999 <katieoliviaholliday@gmail.com>
2023-02-20 18:34:07 +0000

    update intro component

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -3,19 +3,28 @@ import { robotoMono, roboto } from "@/app/fonts";
 
 export default function Intro() {
   return (
-    <header className={styles.intro}>
-      <h1 className={styles.smallHeader}>
-        <span className={robotoMono.className}>Hello my name is</span>
-        <span className={roboto.className + " " + styles.bigHeader}>
-          Katie Holliday
-        </span>
-      </h1>
-      <p className={robotoMono.className + " " + styles.introDescription}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.
-      </p>
-      <button className={styles.button1}>Projects</button>
-      <button className={styles.button2}>LinkedIn</button>
+    <header className={styles.introContainer}>
+      <section className={styles.introText}>
+        <h1 className={styles.smallHeader}>
+          <span className={robotoMono.className}>Hello my name is</span>
+          <span className={roboto.className + " " + styles.bigHeader}>
+            Katie Holliday
+          </span>
+        </h1>
+        <p className={robotoMono.className + " " + styles.introDescription}>
+          I&apos;m a junior front end developer. Welcome to my portfolio.
+        </p>
+
+        <div className={styles.buttonsContainer}>
+          <button className={styles.button1}>Projects</button>
+          <button className={styles.button2}>LinkedIn</button>
+        </div>
+      </section>
+      <section className={styles.introImageContainer}>
+        <div className={styles.introBlob}>
+          <div className={styles.introBlobMini}></div>
+        </div>
+      </section>
     </header>
   );
 }
