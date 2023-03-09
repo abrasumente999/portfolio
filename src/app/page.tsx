c577aee2f283f57b9cc8d743db4e3571c31c8aa2
abrasumente999 <katieoliviaholliday@gmail.com>
2023-03-09 18:33:51 +0000

    update about page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import About from "@/components/About/About";
 import Intro from "../components/Intro/Intro";
 
 export default function Home() {
-  return <Intro />;
+  return (
+    <>
+      <Intro />
+      <About />
+    </>
+  );
 }
--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,1 +1,35 @@
-export default function About() {}
+import styles from "./About.module.css";
+import { robotoMono, roboto } from "@/app/fonts";
+
+export default function About() {
+  return (
+    <section className={styles.aboutContainer}>
+      <div className={styles.aboutTextContainer}>
+        <div className={styles.aboutText}>
+          <h2 className={roboto.className + " " + styles.bigHeader}>
+            About Me
+          </h2>
+          <p className={robotoMono.className + " " + styles.aboutDescription}>
+            I&apos;m a junior front end developer. Welcome to my portfolio.
+            <br />
+            hfbsdjhfbdjhfbdshjm fdsfsdhfbsdhfbsdfhjbds I&apos;m a junior front
+            end developer.
+            <br /> Welcome to my portfolio. hfbsdjhfbdjhfbdshjm
+            <br />
+            fdsfsdhfbsdhfbsdfhjbds I&apos;m a junior front end developer.
+            <br />
+            <br />
+            Welcome to my portfolio. hfbsdjhfbdjhfbdshjm fdsfsdhfbsdhfbsdfhjbds
+          </p>
+        </div>
+        <div className={styles.aboutBlobMini}>
+          <div className={styles.profileImage}></div>
+        </div>
+      </div>
+      <section className={styles.aboutImageContainer}>
+        <div className={styles.profileImage}></div>
+      </section>
+      <div className={styles.aboutBlob}></div>
+    </section>
+  );
+}
