4c21971cfe3a3c74d47058b7ded7ee3cc3e22696
abrasumente999 <katieoliviaholliday@gmail.com>
2023-03-10 16:13:12 +0000

    update about page

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -4,31 +4,25 @@ import { robotoMono, roboto } from "@/app/fonts";
 export default function About() {
   return (
     <section className={styles.aboutContainer}>
-      <div className={styles.aboutTextContainer}>
-        <div className={styles.aboutText}>
-          <h2 className={roboto.className + " " + styles.bigHeader}>
-            About Me
-          </h2>
-          <p className={robotoMono.className + " " + styles.aboutDescription}>
-            I&apos;m a junior front end developer. Welcome to my portfolio.
-            <br />
-            hfbsdjhfbdjhfbdshjm fdsfsdhfbsdhfbsdfhjbds I&apos;m a junior front
-            end developer.
-            <br /> Welcome to my portfolio. hfbsdjhfbdjhfbdshjm
-            <br />
-            fdsfsdhfbsdhfbsdfhjbds I&apos;m a junior front end developer.
-            <br />
-            <br />
-            Welcome to my portfolio. hfbsdjhfbdjhfbdshjm fdsfsdhfbsdhfbsdfhjbds
-          </p>
-        </div>
-        <div className={styles.aboutBlobMini}>
-          <div className={styles.profileImage}></div>
-        </div>
+      <div className={styles.aboutText}>
+        <h2 className={roboto.className + " " + styles.bigHeader}>About Me</h2>
+        <p className={robotoMono.className + " " + styles.aboutDescription}>
+          I&apos;m a junior front end developer. Welcome to my portfolio.
+          <br />
+          hfbsdjhfbdjhfbdshjm fdsfsdhfbsdhfbsdfhjbds I&apos;m a junior front end
+          developer.
+          <br /> Welcome to my portfolio. hfbsdjhfbdjhfbdshjm
+          <br />
+          fdsfsdhfbsdhfbsdfhjbds I&apos;m a junior front end developer.
+        </p>
       </div>
-      <section className={styles.aboutImageContainer}>
+      <div className={styles.aboutBlobMini}>
         <div className={styles.profileImage}></div>
-      </section>
+      </div>
+
+      <div className={styles.profileImage}></div>
+
+      <div className={styles.aboutImageContainer}></div>
       <div className={styles.aboutBlob}></div>
     </section>
   );
