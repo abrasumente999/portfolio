42fedf30c979813c79fbc7ff002cc062d9fa9141
abrasumente999 <katieoliviaholliday@gmail.com>
2023-03-13 18:20:40 +0000

    update page to display projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import About from "@/components/About/About";
 import Intro from "../components/Intro/Intro";
+import Projects from "@/components/Projects/Projects";
 
 export default function Home() {
   return (
     <>
       <Intro />
       <About />
+      <Projects />
     </>
   );
 }
