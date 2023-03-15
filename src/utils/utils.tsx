b2896f57397371143231df9a6ef52067f2b68db6
abrasumente999 <katieoliviaholliday@gmail.com>
2023-03-15 17:43:34 +0000

    fixes type error when id comes through as a string

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -4,9 +4,9 @@ import cover2 from "../../public/cover2.jpg";
 import cover3 from "../../public/cover3.jpg";
 
 export const images: StaticImageData[] = [cover1, cover2, cover3];
-console.log(images);
-const imageByIndex = (index: number): string => {
-  return images[index % images.length].src;
+
+const imageByIndex = (index: number | string): string => {
+  return images[+index % images.length].src;
 };
 
 export default imageByIndex;
