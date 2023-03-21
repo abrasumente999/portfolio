9c30bdff8314d81348c45dbd1dbb52198b47cf33
abrasumente999 <katieoliviaholliday@gmail.com>
2023-03-21 15:00:26 +0000

    remove fonts

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -1,5 +1,5 @@
 import styles from "./Intro.module.css";
-import { robotoMono, roboto } from "@/app/fonts";
+import { robotoMono } from "@/app/fonts";
 
 export default function Intro() {
   return (
@@ -9,24 +9,16 @@ export default function Intro() {
           <span className={robotoMono.className + " " + styles.smallHeader}>
             Hello my name is
           </span>
-          <span className={roboto.className + " " + styles.bigHeader}>
-            Katie
-          </span>
-          <span className={roboto.className + " " + styles.bigHeader}>
-            Holliday
-          </span>
+          <span className={styles.bigHeader}>Katie</span>
+          <span className={styles.bigHeader}>Holliday</span>
         </h1>
         <p className={robotoMono.className + " " + styles.introDescription}>
           I&apos;m a junior front end developer. Welcome to my portfolio.
         </p>
 
         <div className={styles.buttonsContainer}>
-          <button className={roboto.className + " " + styles.button1}>
-            Projects
-          </button>
-          <button className={roboto.className + " " + styles.button2}>
-            LinkedIn
-          </button>
+          <button className={styles.button1}>Projects</button>
+          <button className={styles.button2}>LinkedIn</button>
         </div>
       </section>
       <section className={styles.introImageContainer}>
