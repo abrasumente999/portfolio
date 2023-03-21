091f10cedd96752e10fc7d3413085565de9b073e
abrasumente999 <katieoliviaholliday@gmail.com>
2023-03-21 15:00:36 +0000

    remove fonts

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,11 +1,11 @@
 import styles from "./About.module.css";
-import { robotoMono, roboto } from "@/app/fonts";
+import { robotoMono } from "@/app/fonts";
 
 export default function About() {
   return (
     <section className={styles.aboutContainer} id="about">
       <div className={styles.aboutText}>
-        <h2 className={roboto.className + " " + styles.bigHeader}>About Me</h2>
+        <h2 className={styles.bigHeader}>About Me</h2>
         <p className={robotoMono.className + " " + styles.aboutDescription}>
           I&apos;m a junior front end developer. Welcome to my portfolio.
           <br />
